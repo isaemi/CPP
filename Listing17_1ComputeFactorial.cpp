8ecf3ec4872d31856d13cf04b4b6bb919e32e4cb
isaemi <123706493+isaemi@users.noreply.github.com>
2023-05-22 17:30:14 -0700

    Create Listing17_1ComputeFactorial.cpp

---
+++ Listing17_1ComputeFactorial.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+// Return the factorial for a specified index
+int factorial(int);
+
+int main()
+{
+    // Prompt the user the enter an integer
+    cout << "Please enter a non-negative integer: ";
+    int n;
+    cin >> n;
+
+    // Display factorial
+    cout << "Factorial of " << n << " is " << factorial(n);
+    return 0;
+}
+
+
+int factorial(int n)
+{
+    if(n == 0) // Base case
+        return 1;
+    else
+        return n * factorial(n - 1); // Recursive call
+}
