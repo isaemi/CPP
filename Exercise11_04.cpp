e095ef5795ba0674760b94f74e360c198b0398ff
isaemi <123706493+isaemi@users.noreply.github.com>
2023-05-27 13:59:20 -0700

    Create Exercise11_04.cpp

---
+++ Exercise11_04.cpp
@@ -0,0 +1,53 @@
+/*
+(Sum of even locations) Write two overloaded functions that return the sum of
+values at even locations of an array with the following headers:
+int sumOfEven(const int* array, int size);
+double sumOfEven(const double* array, int size);
+Write a test program that reads five integers or double values,
+invokes this function, and displays the sum of values at even locations.
+*/
+
+#include <iostream>
+using namespace std;
+
+int sumOfEven(const int* array, int size);
+double sumOfEven(const double* array, int size);
+
+void deleteArray(int* arr){
+    delete[] arr;
+    arr = nullptr;
+}
+
+int main(){
+    int intArr[5];
+    double doubleArr[5];
+
+    cout << "Enter 5 integers: ";
+    for(int i = 0; i < 5; i++){
+        cin >> intArr[i];
+    }
+    cout << "Sum of values at even locations(int): " << sumOfEven(intArr, 5) << endl;
+    
+    cout << "Enter 5 doubles: ";
+    for(int i = 0; i < 5; i++){
+        cin >> doubleArr[i];
+    }
+    cout << "Sum of values at even locations(double): " << sumOfEven(doubleArr, 5) << endl;
+    return 0;
+}
+
+int sumOfEven(const int* array, int size){
+    int sum = 0;
+    for(int i = 1; i < size; i += 2){
+        sum += array[i];
+    }
+
+    return sum;
+}
+double sumOfEven(const double* array, int size){
+    double sum = 0.0;
+    for(int i = 1; i < size; i +=2 ){
+        sum += array[i];
+    }
+    return sum;
+}
