4fd9ffa2e9b42d721c2aa7dd41796777e02cc352
isaemi <123706493+isaemi@users.noreply.github.com>
2023-06-20 22:55:01 -0700

    Create w3resource_04.cpp

---
+++ w3resource_04.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a;
+    int b;
+    int sum;
+    a = 29;
+    b = 30;
+    sum = a + b;
+    cout << "The sum of " << a << " and " << b << " is : " << sum;
+    return 0;
+}
