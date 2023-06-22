5f11f8d2748c2e3ff30b42a071f7f5de9a836372
isaemi <123706493+isaemi@users.noreply.github.com>
2023-06-22 13:19:05 -0700

    Create w3resource_06.cpp

---
+++ w3resource_06.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    char Gender = 'F';
+    bool isMarried = true;
+    unsigned short numOfSon = 2;
+    short nearOfAppointment = 2009;
+    unsigned int Salary = 1500000;
+    double Height = 79.48;
+    float GPA = 4.69;
+    long SalaryDrawn = 12047235;
+    long long Balance = 995324987;
+
+    cout << Gender << endl;
+    cout << isMarried << endl;
+    cout << numOfSon << endl;
+    cout << nearOfAppointment << endl;
+    cout << Salary << endl;
+    cout << Height << endl;
+    cout << GPA << endl;
+    cout << SalaryDrawn << endl;
+    cout << Balance << endl;
+    return 0;
+}
