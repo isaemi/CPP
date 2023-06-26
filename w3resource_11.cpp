76b3b5e39d1a5783818317a2e2984e4d6f5543af
isaemi <123706493+isaemi@users.noreply.github.com>
2023-06-25 22:46:05 -0700

    Create w3resource_11.cpp

---
+++ w3resource_11.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "Print the result of some specific operation" << endl;
+    cout << "--------------------------------------------" << endl;
+    cout << "Result of 1st expression is: " << (11*2)+1 << endl;
+    cout << "Result of 2nd expression is: " << (25%3)*5 << endl;
+    cout << "Result of 3rd expression is: " << (27-4)/2 << endl;
+    cout << "Result of 4th expression is: " << (15+3)%5 << endl;
+
+    return 0;
+}
