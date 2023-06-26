c2e15f9338316452e4df3ce519dac6074ef0a601
isaemi <123706493+isaemi@users.noreply.github.com>
2023-06-25 22:57:22 -0700

    Create w3resource_13.cpp

---
+++ w3resource_13.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+   cout << "Swap two numbers:" << endl;
+   cout << "-----------------" << endl;
+   int n1, n2, temp;
+   cout << "Input 1st number: ";
+   cin >> n1;
+   cout << "Input 2nd number: ";
+   cin >> n2;
+   temp = n1;
+   n1 = n2;
+   n2 = temp;
+   cout << "After swapping the 1st number is: " << n1 << endl;
+   cout << "After swapping the 2nd number is: " << n2 << endl;
+}
