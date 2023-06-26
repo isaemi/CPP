36994628516c0de5e1e4b774742610358e9119eb
isaemi <123706493+isaemi@users.noreply.github.com>
2023-06-25 22:52:43 -0700

    Create w3resource_12.cpp

---
+++ w3resource_12.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n1, n2;
+    cout << "Sum of two numbers: " << endl;
+    cout << "-------------------" << endl;
+    cout << "Input 1st number: " << endl;
+    cin >> n1;
+    cout << "Input 2nd number: " << endl;
+    cin >> n2;
+    int sum = n1 + n2;
+    cout << "The sum of the number is " << sum << endl;
+    return 0;
+}
