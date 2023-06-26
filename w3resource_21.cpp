2d78cf22672f43fea9353c3b74b9ee3c279a41a4
isaemi <123706493+isaemi@users.noreply.github.com>
2023-06-26 15:52:03 -0700

    Create w3resource_21.cpp

---
+++ w3resource_21.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "Convert temperature in Fahrenheit to Celsius: " << endl;
+    cout << "----------------------------------------------" << endl;
+    cout << "Input the temperature in Fahrenheit: ";
+    int Cels, Fahr;
+    cin >> Fahr;
+    Cels = (Fahr - 32) / 1.8;
+    cout << "The temperature in Fahrenheit: " << Fahr << endl;
+    cout << "The temperature in Celsius: " << Cels << endl;
+    
+    return 0;
+}
