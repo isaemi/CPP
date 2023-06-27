67001d8df74f0a8d3f2425a651307d75a2f2c49c
isaemi <123706493+isaemi@users.noreply.github.com>
2023-06-26 23:04:28 -0700

    Create w3resource_31.cpp

---
+++ w3resource_31.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "Make a rectangular shape by a single digit number:" << endl;
+    cout << "--------------------------------------------------" << endl;
+    cout << "Input the number: ";
+    int x;
+    cin >> x;
+    cout << x << x << x << x << endl;
+    cout << x << "  " << x << endl;
+    cout << x << "  " << x << endl;
+    cout << x << "  " << x << endl;
+    cout << x << "  " << x << endl;
+    cout << x << x << x << x << endl;
+
+    return 0;
+}
