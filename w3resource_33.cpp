986ecb02314201c35542fb950b9259a99875e23f
isaemi <123706493+isaemi@users.noreply.github.com>
2023-06-27 00:11:34 -0700

    Create w3resource_33.cpp

---
+++ w3resource_33.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "Divide two numbers and print:" << endl;
+    cout << "-----------------------------" << endl;
+    int n1, n2, quotient;
+    cin >> n1 >> n2;
+    quotient = n1 / n2;
+    cout << "The quotient of " << n1 << " and " << n2 << " is: " << quotient << endl;
+
+
+    return 0;
+}
