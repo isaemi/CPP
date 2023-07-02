666f9404924cc7192f3bf66dc7bea8ffe1f0fbfc
isaemi <123706493+isaemi@users.noreply.github.com>
2023-07-02 16:44:25 -0700

    Create w3resource_35.cpp

---
+++ w3resource_35.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "Compute the specified expressions and print the output" << endl;
+    cout << "------------------------------------------------------" << endl;
+    cout << "Result of the expression (" << "25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5) is : " << (25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5);
+
+    return 0;
+}
