04bea17e6e2a57e0e216f0fcce59291839cf3c1b
isaemi <123706493+isaemi@users.noreply.github.com>
2023-07-03 17:20:17 -0700

    Create w3resource_39.cpp

---
+++ w3resource_39.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+    cout << " xxxxx\n";
+    cout << "x     x      x       x\n";
+    cout << "x     x      x       x\n";
+    cout << "x         xxxxxxx xxxxxxx\n";
+    cout << "x            x       x\n";
+    cout << "x     x      x       x\n";
+    cout << " xxxxx\n";
+
+    return 0;
+}
